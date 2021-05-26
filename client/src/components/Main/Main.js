5d67fd760367e2ee6b9cc6e1ce8c9e6b1c55e07d
Daniel San Eusebio Tejedor <daniel@sirkkaltd.com>
2021-05-26 18:29:45 +0100

    #20 Add error handle in Main

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -9,10 +9,35 @@ function Main() {
   useObserver(db);
 
   useEffect(() => {
-    getNewsTitles("category").then((newsTitles) => (db._newsTitle = newsTitles));
+    getNewsTitles("category").then((data) => {
+      if (data.error) {
+        db._newsTitleError = data.error;
+      } else {
+        db._newsTitle = data;
+      }
+    });
   }, []);
 
-  return <div>Hello from Main</div>;
+  return (
+    <div>
+      {db._newsTitle.length === 0 && !db._newsTitleError ? (
+        "loading"
+      ) : db._newsTitleError ? (
+        "error"
+      ) : (
+        <div>
+          {db._newsTitle.map((title, i) => {
+            console.log(title);
+            return (
+              <div key={i}>
+                <h3>{title}</h3>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
