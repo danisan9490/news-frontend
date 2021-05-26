6d3781d227f131a6a702ebdd166e5c9d345c1c5b
Daniel San Eusebio Tejedor <daniel@sirkkaltd.com>
2021-05-26 16:42:21 +0100

    #11 Create routing paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import ContinueReading from "./components/ContinueReading/ContinueReading";
 import Main from "./components/Main/Main";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Main />
-      <ContinueReading />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/continueReading" component={ContinueReading} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
