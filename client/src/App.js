e0625a4c26db95a89d9fb7be856359b58e57cd29
Daniel San Eusebio Tejedor <daniel@sirkkaltd.com>
2021-05-26 16:58:50 +0100

    #13 Create global Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,29 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <div className="fixed-top shadow bg-primary">
-        <div className="formContainer px-4">
+      <div className="px-3 py-2 fixed-top shadow bg-primary d-flex dlex-row">
+        <div className="formContainer">
           <Link to="/" className="my-2">
             <button type="button" className="btn btn-primary">
-              <i className="fas fa-arrow-left"></i> Back to main
+              <i class="fas fa-home"></i> Main
+            </button>
+          </Link>
+        </div>
+        <div className="formContainer">
+          <Link to="/continueReading" className="my-2">
+            <button type="button" className="btn btn-primary">
+              <i class="fas fa-book-reader"></i> Continue Reading
             </button>
           </Link>
         </div>
       </div>
 
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/continueReading" component={ContinueReading} />
-      </Switch>
+      <div className="my-5 pt-4">
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/continueReading" component={ContinueReading} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
