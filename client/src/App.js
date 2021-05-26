edc38798dfb8eef520fd677c70e91cb35e8bb55d
Daniel San Eusebio Tejedor <daniel@sirkkaltd.com>
2021-05-26 16:49:04 +0100

    #12 Add Fontawesome CDN

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import ContinueReading from "./components/ContinueReading/ContinueReading";
 import Main from "./components/Main/Main";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Main} exact />
-          <Route path="/continueReading" component={ContinueReading} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="fixed-top shadow bg-primary">
+        <div className="formContainer px-4">
+          <Link to="/" className="my-2">
+            <button type="button" className="btn btn-primary">
+              <i className="fas fa-arrow-left"></i> Back to main
+            </button>
+          </Link>
+        </div>
+      </div>
+
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/continueReading" component={ContinueReading} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
