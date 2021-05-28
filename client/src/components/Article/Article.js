19ef860a0c133f69961b689ce1abc3620b673684
Daniel San Eusebio Tejedor <daniel@sirkkaltd.com>
2021-05-28 18:31:05 +0100

    #26 Add get id from path in Article

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//Helpers
+import { useHistory } from "react-router-dom";
+import db from "../../POJO/POJO";
+import useObserver from "pojo-observer";
 
 function Article() {
+  useObserver(db);
+  let history = useHistory();
+  const [articleId, setArticleId] = useState(history.location.pathname.split("/")[2]);
+  const [article, setArticle] = useState({});
+
+  useEffect(() => {
+    console.log(articleId);
+  }, [articleId]);
+
   return <div>Continue Reading</div>;
 }
 
